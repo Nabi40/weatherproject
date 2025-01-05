c3bd899f824d0210d5b415b01ac04a35747e1700
Nabi40 <mahamudunn2245@gmail.com>
2025-01-05 20:54:43 +0600

    url

--- weatherproject/urls.py
+++ weatherproject/urls.py
@@ -14,21 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path, include
-# from django.conf import settings
-# from django.conf.urls.static import static
-
-# urlpatterns = [
-#     # path('admin/', admin.site.urls),
-#     path('', include('weatherapp.urls')),
-# ] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-
-from django.views.generic import RedirectView
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    # Other URL patterns...
-    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
-]
+    # path('admin/', admin.site.urls),
+    path('', include('weatherapp.urls')),
+] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
+
 
