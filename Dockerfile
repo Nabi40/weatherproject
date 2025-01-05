f03e62da8345ebd465fad93a2a0d58b855723094
Nabi40 <mahamudunn2245@gmail.com>
2025-01-05 20:51:08 +0600

    url

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,16 @@ FROM python:3.10
 
 WORKDIR /app
 
+RUN python -m venv /opt/venv
+
+
+ENV PATH="/opt/venv/bin:$PATH"
+
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
 EXPOSE 8000
-
-RUN python manage.py collectstatic --noinput
 
 CMD ["gunicorn", "weatherproject.wsgi:application", "--bind", "0.0.0.0:8000"]
--- weatherproject/urls.py
+++ weatherproject/urls.py
@@ -15,11 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.urls import path, include
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# urlpatterns = [
+#     # path('admin/', admin.site.urls),
+#     path('', include('weatherapp.urls')),
+# ] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
+from django.views.generic import RedirectView
+from django.urls import path
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', include('weatherapp.urls')),
-] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    # Other URL patterns...
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
+]
+
